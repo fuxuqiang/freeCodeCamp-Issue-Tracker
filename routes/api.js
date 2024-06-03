2e866c6301b8fc76677dce5fe549b7349dadb478
Lasse Jørgensen <28780271+lasjorg@users.noreply.github.com>
2024-06-03 22:40:52 +0200

    pass tests

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,98 @@
 'use strict';
 
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI);
+
+const issue = new mongoose.Schema({
+  project: {
+    type: String,
+    required: true
+  },
+  issue_title: {
+    type: String,
+    required: true
+  },
+  issue_text: {
+    type: String,
+    required: true
+  },
+  created_by: {
+    type: String,
+    required: true
+  },
+  assigned_to: {
+    type: String
+  },
+  status_text: {
+    type: String
+  },
+  open: {
+    type: Boolean
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_on',
+    updatedAt: 'updated_on'
+  }
+});
+
+const Issue = mongoose.model('Issue', issue);
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      let project = req.params.project;
-      
+    .get(async (req, res) => {
+      res.json(await Issue.find(Object.assign({ project: req.params.project }, req.query)));
     })
     
-    .post(function (req, res){
-      let project = req.params.project;
-      
+    .post(async (req, res) => {
+      const { issue_title, issue_text, created_by } = req.body;
+      if (!issue_title || !issue_text || !created_by) {
+        return res.json({ error: 'required field(s) missing' });
+      }
+      let issue = new Issue({
+        project: req.params.project,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        created_on: new Date(),
+        updated_on: new Date(),
+        open: true
+      });
+      await issue.save();
+      res.json(issue);
     })
     
-    .put(function (req, res){
-      let project = req.params.project;
-      
+    .put(async (req, res) => {
+      if (!req.body._id) {
+        return res.json({ error: 'missing _id' });
+      }
+      if (Object.keys(req.body).length == 1) {
+        return res.json({ error: 'no update field(s) sent', '_id': req.body._id });
+      }
+      const result = await Issue.updateOne({ _id: req.body._id }, req.body);
+      if (result.matchedCount) {
+        res.json({  result: 'successfully updated', '_id': req.body._id }); 
+      } else {
+        res.json({ error: 'could not update', _id: req.body._id });
+      }
     })
     
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+    .delete(async (req, res) => {
+      if (!req.body._id) {
+        return res.json({ error: 'missing _id' });
+      }
+      const result = await Issue.deleteOne({ _id: req.body._id });
+      if (result.deletedCount) {
+        res.json({ result: 'successfully deleted', '_id': req.body._id });
+      } else {
+        res.json({ error: 'could not delete', '_id': req.body._id });
+      }
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,172 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+    let _id1, _id2;
+    test('Create an issue with every field', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/fcc-project')
+          .send({
+            issue_title: 'Faux Issue Title 2',
+            issue_text: 'Functional Test - Every field filled in',
+            created_by: 'fCC',
+            assigned_to: 'Chai and Mocha',
+            status_text: 'open'
+          })
+          .end((err, res) => {
+            _id1 = res.body._id;
+            assert.isString(_id1);
+            done();
+          });
+    });
+    test('Create an issue with only required fields', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/fcc-project')
+          .send({
+            issue_title: 'Faux Issue Title',
+            issue_text: 'Functional Test - Required Fields Only',
+            created_by: 'fCC'
+          })
+          .end((err, res) => {
+            _id2 = res.body._id;
+            assert.isString(_id2);
+            done();
+          });
+    });
+    test('Create an issue with missing required fields', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/fcc-project')
+          .send({ created_by: 'fCC' })
+          .end((err, res) => {
+            assert.equal(res.body.error, 'required field(s) missing');
+            done();
+          });
+    });
+    test('View issues on a project', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/issues/fcc-project')
+          .end((err, res) => {
+            assert.isArray(res.body);
+            done();
+          });
+    });
+    test('View issues on a project with one filter', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/issues/fcc-project?created_by=fCC')
+          .end((err, res) => {
+            assert.equal(res.body[0].created_by, 'fCC');
+            done();
+          });
+    });
+    test('View issues on a project with multiple filters', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .get('/api/issues/fcc-project?created_by=fCC&status_text=open')
+          .end((err, res) => {
+            assert.equal(res.body[0].created_by, 'fCC');
+            assert.equal(res.body[0].status_text, 'open');
+            done();
+          });
+    });
+    test('Update an issue with missing _id', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .put('/api/issues/fcc-project')
+          .send({ issue_text: 'New Issue Text' })
+          .end((err, res) => {
+            assert.equal(res.body.error, 'missing _id');
+            done();
+          });
+    });
+    test('Update one field on an issue', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .put('/api/issues/fcc-project')
+          .send({ _id: _id1, issue_text: 'New Issue Text' })
+          .end((err, res) => {
+            assert.equal(res.body.result, 'successfully updated');
+            done();
+          });
+    });
+    test('Update multiple fields on an issue', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .put('/api/issues/fcc-project')
+          .send({
+            _id: _id2,
+            issue_text: 'New Issue Text',
+            issue_title: 'Faux Issue Title 3'
+          })
+          .end((err, res) => {
+            assert.equal(res.body.result, 'successfully updated');
+            done();
+          });
+    });
+    test('Update an issue with no fields to update', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .put('/api/issues/fcc-project')
+          .send({ _id: _id1 })
+          .end((err, res) => {
+            assert.equal(res.body.error, 'no update field(s) sent');
+            done();
+          });
+    });
+    test('Update an issue with an invalid _id', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .put('/api/issues/fcc-project')
+          .send({ _id: '674ac53a1a54b5fca17e813b', issue_text: 'New Issue Text' })
+          .end((err, res) => {
+            assert.equal(res.body.error, 'could not update');
+            done();
+          });
+    });
+    test('Delete an issue', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .delete('/api/issues/fcc-project')
+          .send({ _id: _id1 })
+          .end((err, res) => {
+            assert.equal(res.body.result, 'successfully deleted');
+            done();
+          });
+    });
+    test('Delete an issue with an invalid _id', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .delete('/api/issues/fcc-project')
+          .send({ _id: '674ac53a1a54b5fca17e813b' })
+          .end((err, res) => {
+            assert.equal(res.body.error, 'could not delete');
+            done();
+          });
+    });
+    test('Delete an issue with missing _id', (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .delete('/api/issues/fcc-project')
+          .end((err, res) => {
+            assert.equal(res.body.error, 'missing _id');
+            done();
+          });
+    });
 });
